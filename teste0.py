0b510a43c80b7ac186b1c11f4c8b39300400d972
victor <tamashi687@hotmail.com>
2022-10-22 10:40:05 -0300

    1.5

--- teste0.py
+++ teste0.py
@@ -3,24 +3,22 @@
 
 from PySimpleGUI import PySimpleGUI as sg
 
-# Layout
-sg.theme('Reddit')
-layout = [
-    [sg.Text('Usuario:'), sg.Input(key='user')],
-    [sg.Text('senha:  '), sg.Input(key='senha', password_char='#')],
-    [sg.Checkbox('salvar login?')],
-    [sg.Button('entrar')]
-]
+class Inter:
+    def __init__(self):
+        sg.theme('Dark Grey 13')
+        layout = [
+            [sg.Text('Aluno:   '), sg.Input(size=(25, 0), key='nome')],
+            [sg.Text('Numero:'), sg.Input(size=(25, 0), key='numero')],
+            [sg.Text('Qual sua serie?')],
+            [sg.Radio('1A', 'serie', key='A1'), sg.Radio('2A', 'serie', key='A2'), sg.Radio('3A', 'serie', key='A3')],
+            [sg.Text('Qual a materia?')],
+            [sg.Radio('portugues', 'materia', key='port'), sg.Radio('matematica', 'materia', key='mat')],
+            [sg.Button('entrar')]
+        ]
 
-# janela
-janela = sg.Window('login', layout)
+        # janela
+        janela = sg.Window('login').layout(layout)
+        self.button, self.values = janela.read()
 
-# eventos
-while True:
-    eventos, valores = janela.read()
-
-    if eventos == sg.WINDOW_CLOSED:
-        break
-    if eventos == 'entrar':
-        if valores['user'] == 'victor' and valores['senha'] == 1234:
-            print('bem vindo')
+    def iniciar(self):
+        nome = self.values['nome']
